0ff14787026334d6380d5e5fe13ee962e9ec860e
farrel-11 <23502410011@student.prasetiyamulya.ac.id>
2025-10-04 00:53:31 +0700

    Rapihkan struktur  project ke dalam folder UTS-WEB-2025-FARRELRIZQIM

--- coop_app/apps.py
+++
@@ -1,3 +0,0 @@
-from django.apps import AppConfig
-class CoopAppConfig(AppConfig):
-    name = 'coop_app'
--- coop_app/static/coop_app/script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-  console.log('Coop script loaded');
-  // simple client-side interactions can be added here
-});
